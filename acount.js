e8b2654441ac844a41137d5e662d73b7693c9e21
tomzumbi <114890330+tomzumbi@users.noreply.github.com>
2022-10-28 15:37:18 +0700

    Add files via upload

---
+++ acount.js
@@ -0,0 +1,11 @@
+class Account{
+    username;
+    password;
+    fullName;
+
+    constructor(username, password, fullName) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+    }
+}
